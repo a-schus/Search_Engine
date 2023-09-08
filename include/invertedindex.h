9ed309429cff1ead6fd36a7c6f4b2c9b0fa4bc91
a-schus <a.a.schus@gmail.com>
2023-09-08 16:20:55 +0700

    Add comments to InvertedTndex

--- include/invertedindex.h
+++ include/invertedindex.h
@@ -21,26 +21,42 @@ struct Entry {
 
 class InvertedIndex
 {
+    // Содержимое текстовых документов для поиска
     QVector<QString> docs;
+
+    // Содержит количесто повторений слов в каждом документе для поиска
     QMap<QString, QVector<Entry>> freqDictionary;
 
 public:
-    QRegularExpression splitter = QRegularExpression("[^0-9A-Za-zА-Яа-яЁё]"); // регулярное выражение для разделения строки QString на отдельные слова
+    // регулярное выражение для разделения строки QString на отдельные слова
+    QRegularExpression splitter = QRegularExpression("[^0-9A-Za-zА-Яа-яЁё]");
 
     InvertedIndex();
 
-    void setDocs(const QVector<QString>& inputDocs);// одтельный метод для инициализации docs
-                                                    // для более удобного использования (чтобы можно было
-                                                    // изменять список файлтов в процессе выполнения приложения
+    // Одтельный метод для инициализации docs для более удобного использования
+    // чтобы можно было задавать свой список файлтов
+    void setDocs(const QVector<QString>& inputDocs);
+
+    // Формируем freqDictionary в многопоточном режиме
     void UpdateDocumentBase();
+
+    // Формируем freqDictionary в однопоточном режиме. Использовалось для сравнения скорости выполнения
     void UpdateDocumentBaseSingleThread();
+
+    // Возвращает количество повторений определенного слова в каждом документе
     QVector<Entry> getWordCount(const QString& word);
+
+    // Дает возможность установить пользовательский разделитель для парсинга документов
     void setSplitter(const QRegularExpression& splitterRegExp);
 
 private:
     void indexingDocumentSingleThread(const size_t docId); // для сравнения времени выполнения многопоточного и однопоточного вариантов
 
-    QMap<QString, Entry>* indexingDocument(const QString& doc, const size_t docId); // возвращаем указатель, чтобы избавиться от копирования больших объемов данных
+    // Создает freqDictionary для одного конкретного документа и возвращает указатель на него
+    QMap<QString, Entry>* indexingDocument(const QString& doc, const size_t docId);
+
+    // Объединяет все freqDictionary, полученные с помощью
+    // QMap<QString, Entry>* indexingDocument(const QString& doc, const size_t docId)
     void mergeFreqDictionarys(const QMap<QString, Entry> *docFreqDictionary);
 };
 
